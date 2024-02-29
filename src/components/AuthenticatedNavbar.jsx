8e3e7fd9e5806bc21356ae6890a968597c515594
Abby <abigailbernabei@Abigails-Air.lan>
2024-02-28 22:37:09 -0600

    setup

---
+++ src/components/AuthenticatedNavbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthenticatedNavbar = () => {
+  return (
+    <div>
+      <h1>Authenticated Nav Bar</h1>
+    </div>
+  );
+};
+
+export default AuthenticatedNavbar;
---
+++ src/components/GuestNavbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GuestNavbar = () => {
+  return (
+    <div>
+      <h1>Guest Navbar</h1>
+    </div>
+  );
+};
+
+export default GuestNavbar;
