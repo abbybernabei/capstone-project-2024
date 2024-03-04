a0d30d2386c3ec2fc081815823b1a37166389197
Abby <abigailbernabei@Abigails-Air.lan>
2024-03-04 01:44:15 -0600

    feat: sort on all products page

--- src/components/AllProductsPage.jsx
+++ src/components/AllProductsPage.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
+import { fetchAllProducts } from "../api";
 import "./allProducts.css";
 
 const AllProductsPage = ({ products }) => {
+  const [sortedProducts, setSortedProducts] = useState([]);
+
+  useEffect(() => {
+    setSortedProducts(products);
+  }, [products]);
+
+  const sortProducts = (order) => {
+    let sorted = [...products];
+    if (order === "lowToHigh") {
+      sorted.sort((a, b) => a.price - b.price);
+    } else if (order === "highToLow") {
+      sorted.sort((a, b) => b.price - a.price);
+    }
+    setSortedProducts(sorted);
+  };
+
   return (
     <div className="the-products">
-      {products.map((product) => {
+      <div className="sort-dropdown">
+        <select
+          id="sort"
+          onChange={(e) => sortProducts(e.target.value)}
+          defaultValue=""
+        >
+          <option value="" disabled>
+            Sort by:
+          </option>
+          <option value="lowToHigh">Price: low to high</option>
+          <option value="highToLow">Price: high to low</option>
+        </select>
+      </div>
+
+      {sortedProducts.map((product) => {
         return <ProductCard key={product.id} product={product} />;
       })}
     </div>
