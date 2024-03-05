70f7f598ee063d53aa791f057a42e731c031da71
Abby <abigailbernabei@Abigails-Air.lan>
2024-03-04 22:35:36 -0600

    feat: navbar and styling

--- src/components/AuthenticatedNavbar.jsx
+++ src/components/AuthenticatedNavbar.jsx
@@ -1,10 +1,47 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./navbar.css";
 
-const AuthenticatedNavbar = () => {
+const AuthenticatedNavbar = ({ token, setToken }) => {
+  const navigate = useNavigate();
+  const handleLoggoutClick = () => {
+    setToken(null);
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h1>Authenticated Nav Bar</h1>
-    </div>
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/" className="home-link">
+          Home
+        </Link>
+      </div>
+      <ul className="nav-list">
+        <li className="nav-item">
+          {token ? (
+            <>
+              <Link to="/logout" className="link">
+                <button onClick={handleLoggoutClick} className="logout-btn">
+                  Log Out{" "}
+                </button>
+              </Link>
+              <Link to="/carts" className="link">
+                Cart{" "}
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link to="/users" className="link">
+                Register{" "}
+              </Link>
+              <Link to="/auth/login" className="link">
+                Login{" "}
+              </Link>
+            </>
+          )}
+        </li>
+      </ul>
+    </nav>
   );
 };
 
