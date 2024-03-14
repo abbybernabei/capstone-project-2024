a9e2c3f2ccc4e380ee2adcf99a63debd283020f9
Abby <abigailbernabei@Abigails-Air.lan>
2024-03-13 20:08:39 -0500

    style: cart page

--- src/components/CartItemCard.jsx
+++ src/components/CartItemCard.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const CartItemCard = ({ cartItem, quantity }) => {
+  if (!cartItem) {
+    return <h1>cart empty</h1>;
+  }
+
   return (
     <div>
       <div>
-        <img src={cartItem.image} alt={cartItem.title} />
+        <img src={cartItem?.image} alt={cartItem?.title} />
       </div>
       <div>
         <h3>{cartItem?.title}</h3>
